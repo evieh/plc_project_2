a23596a21184d2715d9316ffc5500cda2e3eac37
Zellenon <Zellenon@gmail.com>
2022-04-27 19:45:25 -0500

    formatting

--- tup/Tup.hs
+++ tup/Tup.hs
@@ -2,22 +2,14 @@ module Tup where
 
 data Pattern = Var String | Null | Tup_Expr [Expression]
 
-data Expression =
---    = Var_Expr String
---    | Tup_Expr [Expression] 
-    Func_Expr String [ Expression ] 
+data Expression
+    = Func_Expr String [ Expression ]
     | Value Pattern
---    | Null_Expr
-    | Int_Expr Int 
+    | Int_Expr Int
     | Operator_Expr Operator Expression Expression
 
--- data Value = Var String | Null | Tup_Expr [Expression]
 
 data Operator = Add | Sub | Mul
     deriving (Eq, Ord, Show)
 
 
--- type FuncDec = String [Pattern] Expression
-    --deriving show
-
--- change this to things like pattern and expressions
