0b3562a36dd140b83ae19569fe87cc853925de50
Zellenon <Zellenon@gmail.com>
2022-04-27 20:03:53 -0500

    FWC, hopefully

--- tup/Tup.hs
+++ tup/Tup.hs
@@ -1,13 +1,22 @@
 module Tup where
 
-data Pattern = Var String | Null | Tup_Expr [Expression]
+data Pattern = PatVar Var | Null_Pat Null | Tup_Var [Var]
+    deriving (Eq, Show)
 
 data Expression
-    = Func_Expr String [ Expression ]
-    | Value Pattern
+    = Func_Call Var [Expression]
+    | Var_Expr Var
+    | Tup_Expr [Expression]
     | Int_Expr Int
-    | Operator_Expr Operator Expression Expression
+    | Null_Expr Null
+    | Op_Expr Expression Operator Expression
+    deriving (Eq, Show)
 
+data Var = Var String
+    deriving (Eq, Show)
+
+data Null = Null
+    deriving (Eq, Show)
 
 data Operator = Add | Sub | Mul
     deriving (Eq, Ord, Show)
