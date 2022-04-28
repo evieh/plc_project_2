aea8fecadb102774b39221ad17af37276cdb1e25
evieh <evholyoake@gmail.com>
2022-04-27 19:39:18 -0500

    Tup data module

--- tup/Tup.hs
+++ tup/Tup.hs
@@ -1,10 +1,23 @@
 module Tup where
 
-data Pattern = Var | ( [ Var ] ) | "#"
+data Pattern = Var String | Null | Tup_Expr [Expression]
 
-data Expression = ( [Expression, Expression] ) | ( Expression ) | Int | 
+data Expression =
+--    = Var_Expr String
+--    | Tup_Expr [Expression] 
+    Func_Expr String [ Expression ] 
+    | Value Pattern
+--    | Null_Expr
+    | Int_Expr Int 
+    | Operator_Expr Operator Expression Expression
 
-type Tup = Pattern | Expression
+-- data Value = Var String | Null | Tup_Expr [Expression]
+
+data Operator = Add | Sub | Mul
+    deriving (Eq, Ord, Show)
+
+
+-- type FuncDec = String [Pattern] Expression
     --deriving show
 
 -- change this to things like pattern and expressions
