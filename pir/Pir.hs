f0c13654290a12479179f89ba372527734808994
Zellenon <Zellenon@gmail.com>
2022-05-10 17:23:12 -0500

    fixed some errors

--- pir/Pir.hs
+++ pir/Pir.hs
@@ -1,9 +1,10 @@
 module Pir where
 
-data Prog = Prog [FuncDec] [Initializer] Expression Expression
+data Prog = Prog [FuncDec] [Statement] Expression Expression
     deriving (Eq, Show)
 
 data Instruction = Statement Statement | Expression Expression | Reference Reference
+    deriving (Eq, Show)
 
 data Expression
     = Func_Call Var Expression Expression
@@ -11,6 +12,7 @@ data Expression
     | Tup_Expr [Expression]
     | Int_Expr Int
     | Null_Expr Null
+    | NullTest_Expr Expression
     | Op_Expr Expression Operator Expression
     deriving (Eq, Show)
 
@@ -18,8 +20,13 @@ data Operator = Add | Sub | Mul
     deriving (Eq, Ord, Show)
 
 data Statement
+  = If Expression [Instruction] [Instruction]
+  | Assignment Reference Expression
+  | Return Expression
+    deriving (Eq, Show)
 
-data Reference
+data Reference = Ref Var | SubRef Var Int
+    deriving (Eq, Show)
 
 type Var = String
 
@@ -27,3 +34,4 @@ data Null = Null
     deriving (Eq, Show)
 
 data FuncDec = FuncDec Var Var [Instruction]
+    deriving (Eq, Show)
